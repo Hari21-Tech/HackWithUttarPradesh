9fba25c559a639d6486e8afdcfeb752bd0d059f5
Hari21-Tech <sri21harisesh@gmail.com>
2025-11-01 10:54:13 +0530

    Intial Setup

---
+++ electron-app/electron/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  const devServer = process.env.VITE_DEV_SERVER_URL;
+  if (devServer) win.loadURL(devServer);
+  else win.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
