656b633ca6817318a81c6e4cdeb336b3e5e02a35
Hari21-Tech <sri21harisesh@gmail.com>
2025-11-01 13:57:54 +0530

    fixes

--- electron-app/electron/main.ts
+++ electron-app/electron/main.ts
@@ -1,22 +1,33 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { app, BrowserWindow } from "electron";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 1000,
+    height: 700,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.ts"),
     },
   });
 
-  const devServer = process.env.VITE_DEV_SERVER_URL;
-  if (devServer) win.loadURL(devServer);
-  else win.loadFile(path.join(__dirname, '../dist/index.html'));
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(join(__dirname, "dist", "index.html"));
+  }
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
